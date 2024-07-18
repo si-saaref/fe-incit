9cd76c14240477f9b382ff14737d75c9ba218e87
si-saaref <achmadsyarif241@gmail.com>
2024-07-18 22:24:34 +0700

    feat: slicing form sign in

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,3 +1,61 @@
+import { useState } from 'react';
+import { LuEye } from 'react-icons/lu';
+import { LuEyeOff } from 'react-icons/lu';
+
 export default function SignIn() {
-	return <div className=''>Sign In</div>;
+	const [inputEmail, setInputEmail] = useState('');
+	const [inputPassword, setInputPassword] = useState('');
+	const [isShowPassword, setIsShowPassword] = useState(false);
+
+	const handleClickPreviewPassword = () => {
+		setIsShowPassword((prevValue) => !prevValue);
+	};
+
+	return (
+		<div className='flex justify-center items-center h-screen'>
+			<div className='form-wrapper shadow-md bg-white h-3/5 w-1/4 p-5 rounded-xl flex flex-col'>
+				<h1 className='text-3xl'>Sign In</h1>
+
+				<div className='content-form-wrapperm flex-1 flex flex-col items-center justify-center gap-3'>
+					<div className='w-full'>
+						<input
+							className='input'
+							type='text'
+							placeholder='Email'
+							value={inputEmail}
+							onChange={(e) => {
+								setInputEmail(e.target.value);
+							}}
+						/>
+					</div>
+					<div className='w-full relative'>
+						<input
+							className='input'
+							type={isShowPassword ? 'text' : 'password'}
+							placeholder='Password'
+							value={inputPassword}
+							onChange={(e) => {
+								setInputPassword(e.target.value);
+							}}
+						/>
+						{!isShowPassword ? (
+							<LuEye
+								className='absolute top-2 right-3 text-slate-500'
+								onClick={handleClickPreviewPassword}
+							/>
+						) : (
+							<LuEyeOff
+								className='absolute top-2 right-3 text-slate-500'
+								onClick={handleClickPreviewPassword}
+							/>
+						)}
+					</div>
+					<button className='btn w-full !rounded-lg'>Sign In</button>
+					<div className='relative w-full flex justify-center'>
+						<p className='separator'>or</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
