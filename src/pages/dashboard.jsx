a057cf41bf89cf1e8a88963b3b93d21034d9c34d
si-saaref <achmadsyarif241@gmail.com>
2024-07-20 23:28:16 +0700

    feat: fix dashboard navbar

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,26 +2,53 @@ import { FacebookLoginClient } from '@greatsumini/react-facebook-login';
 import { googleLogout } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 import { authSignOut } from '../services';
+import { IoLogOutOutline } from 'react-icons/io5';
+import { FaUserCircle } from 'react-icons/fa';
+import { FiUser } from 'react-icons/fi';
+import { IoIosArrowDown } from 'react-icons/io';
+import { useState } from 'react';
 
 export default function Dashboard() {
 	const navigate = useNavigate();
+	const [isShowDropdown, setIsShowDropdown] = useState(false);
+
 	return (
 		<div className=''>
 			<header className='flex justify-end p-5 px-12 shadow-md bg-white'>
 				<div className='flex gap-5'>
-					<button
-						className='btn'
-						onClick={async () => {
-							await authSignOut();
-							googleLogout();
-							FacebookLoginClient.logout(() => {
-								console.log('logout completed!');
-							});
-							navigate('/');
-						}}
+					<div
+						className='relative icon-profile-wrapper flex items-center gap-1 cursor-pointer'
+						onClick={() => setIsShowDropdown((prevState) => !prevState)}
 					>
-						Logout
-					</button>
+						<FaUserCircle className='text-3xl' />
+						<IoIosArrowDown
+							className={`text-gray-400 ${isShowDropdown ? 'rotate-180' : ''} duration-300`}
+						/>
+
+						<ul
+							className={`dropdown-profile absolute top-10 -right-1 ${
+								isShowDropdown ? 'block' : 'hidden'
+							}`}
+						>
+							<li>
+								<FiUser />
+								<p>My Profile</p>
+							</li>
+							<li
+								onClick={async () => {
+									await authSignOut();
+									googleLogout();
+									FacebookLoginClient.logout(() => {
+										console.log('logout completed!');
+									});
+									navigate('/');
+								}}
+							>
+								<IoLogOutOutline className='text-red-600' />
+								<p className='text-red-600'>Log Out</p>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</header>
 			<main className='h-full p-14 flex flex-col gap-10'>
